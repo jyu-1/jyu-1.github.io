2a83e7603aa3256f4875bca6a52ca78cda916a92
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-04 23:28:53 -0800

    add list of projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,25 @@
 import { forwardRef } from "react";
 import "../styles/project.scss";
+import pData from "./data";
 
 const Project = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className="project">
-            Project
+            <div>Projects: </div>
+            {pData.map((item, index) => {
+                return (
+                    <div
+                        key={index}
+                        className={index % 2 === 0 ? "left-gif" : "right-gif"}
+                    >
+                        <div>{item.name}</div>
+                        <div>{item.description}</div>
+                        <div>{item.preview}</div>
+                        <div>{item.source}</div>
+                        <div>{item.gif}</div>
+                    </div>
+                );
+            })}
         </div>
     );
 });
---
+++ src/components/data.tsx
@@ -0,0 +1,54 @@
+interface Project {
+    name: string;
+    description: string;
+    preview: string;
+    source: string;
+    gif: string;
+}
+
+const pData: Project[] = [
+    {
+        name: "Discuss App",
+        description: "A clone of the popular communication app, Discord.",
+        preview: "https://jyu-1.github.io/discuss/",
+        source: "https://github.com/jyu-1/discuss",
+        gif: "gif",
+    },
+    {
+        name: "Shopping Cart",
+        description: "Front-end solution for a shopping website.",
+        preview: "https://jyu-1.github.io/shopping-cart/",
+        source: "https://github.com/jyu-1/shopping-cart",
+        gif: "gif",
+    },
+    {
+        name: "Pokemon Finder",
+        description: "An adoptation of the Where's Waldo game.",
+        preview: "https://jyu-1.github.io/wheres-waldo/",
+        source: "https://github.com/jyu-1/wheres-waldo",
+        gif: "gif",
+    },
+    {
+        name: "Weather App",
+        description: "Weather app to check forecast of the next five days.",
+        preview: "https://jyu-1.github.io/weather/",
+        source: "https://github.com/jyu-1/weather",
+        gif: "gif",
+    },
+    {
+        name: "Resume Maker",
+        description: "An easy solution to making personal resume.",
+        preview: "https://jyu-1.github.io/cv-project/",
+        source: "https://github.com/jyu-1/cv-project",
+        gif: "gif",
+    },
+    {
+        name: "Memory Card",
+        description: "Select a card without picking the same one.",
+        preview: "https://jyu-1.github.io/memory-card/",
+        source: "https://github.com/jyu-1/memory-card",
+        gif: "gif",
+    },
+];
+
+export default pData;
