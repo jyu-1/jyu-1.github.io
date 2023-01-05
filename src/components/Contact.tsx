a94282caf457bfeff7fb139719bb59f30e65d488
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-04 22:33:32 -0800

    update basic contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,11 @@ import "../styles/contact.scss";
 const Contact = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className="contact">
-            Contact
+            <div>
+                For business inquiries, or if you just want to chat, feel free
+                to send me a message and I will get back to you!
+            </div>
+            <div>My email: jia@nateyu.com</div>
         </div>
     );
 });
