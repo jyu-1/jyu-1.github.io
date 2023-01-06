4bf5e6699a6b2bb632e6372e0e2063b2b1945ba9
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-06 12:33:20 -0800

    add carousel

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,40 +1,38 @@
 import { forwardRef } from "react";
 import "../styles/about.scss";
-import sData from "./skills";
 import Cloud from "./Cloud";
+import Carousel from "./Carousel";
 
 const About = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className="about">
             <div>
-                Hi, I'm Jia, an Electrical Engineering graduate from the
-                University of California, Riverside. When I first learned about
-                web development, I was fascinated by its similarities to my
-                major and the endless possibilities it offers. With my
-                background in research and collaboration with others, I bring a
-                strong foundation in problem-solving and critical thinking to my
-                work in software development.
+                <div>
+                    Hi, I'm Jia, an Electrical Engineering graduate from the
+                    University of California, Riverside. When I first learned
+                    about web development, I was fascinated by its similarities
+                    to my studies and the endless possibilities it offers. With
+                    my background in research and collaboration with others, I
+                    bring a strong foundation in problem-solving and critical
+                    thinking to my work in software development.
+                </div>
+                <div>
+                    Driven by curiosity and a desire to explore new
+                    technologies, I'm constantly finding new ways to build new
+                    experiences. Whether I'm experimenting with new frameworks
+                    or diving into the latest APIs, I'm always looking for ways
+                    to push the boundaries of what's possible with web
+                    development.
+                </div>
+                <div>
+                    Outside of coding, I enjoy playing tennis, trying new foods,
+                    and traveling.
+                </div>
             </div>
-            <div>
-                Driven by curiosity and a desire to explore new technologies,
-                I'm constantly finding new ways to build new experiences.
-                Whether I'm experimenting with new frameworks or diving into the
-                latest APIs, I'm always looking for ways to push the boundaries
-                of what's possible with web development.
+            <div className="about-skills">
+                <Cloud />
+                <Carousel />
             </div>
-            <div>
-                Outside of coding, I enjoy playing tennis, trying new foods, and
-                traveling.
-            </div>
-            {sData.map((item, index) => {
-                return (
-                    <div key={index} className="skill-list">
-                        <img src={item.svg} alt={item.name} />
-                        <div>{item.name}</div>
-                    </div>
-                );
-            })}
-            <Cloud />
         </div>
     );
 });
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,9 +4,9 @@ import ReactSVG from "../assets/icons/react.svg";
 import NodeSVG from "../assets/icons/nodejs.svg";
 import MongoDBSVG from "../assets/icons/mongodb.svg";
 import ExpressSVG from "../assets/icons/express.svg";
+import SASSSVG from "../assets/icons/sass.svg";
 import HTMLSVG from "../assets/icons/html5.svg";
 import CSSSVG from "../assets/icons/css3.svg";
-import SASSSVG from "../assets/icons/sass.svg";
 import GitSVG from "../assets/icons/git.svg";
 import NPMSVG from "../assets/icons/npm.svg";
 import JestSVG from "../assets/icons/jest.svg";
@@ -24,9 +24,9 @@ const sData: Skills[] = [
     { name: "Node.js", svg: NodeSVG },
     { name: "MongoDB", svg: MongoDBSVG },
     { name: "Express.js", svg: ExpressSVG },
+    { name: "SASS", svg: SASSSVG },
     { name: "HTML", svg: HTMLSVG },
     { name: "CSS", svg: CSSSVG },
-    { name: "SASS", svg: SASSSVG },
     { name: "Git", svg: GitSVG },
     { name: "NPM", svg: NPMSVG },
     { name: "Jest", svg: JestSVG },
