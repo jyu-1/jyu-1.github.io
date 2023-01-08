ff92875b59699647a836b8e440fa4a3507620468
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-07 22:32:52 -0800

    add project section styles

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,17 +5,20 @@ import pData from "./data";
 const Project = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className="project">
-            <div>Projects: </div>
             {pData.map((item, index) => {
                 return (
                     <div
                         key={index}
                         className={index % 2 === 0 ? "left-gif" : "right-gif"}
                     >
-                        <div>{item.name}</div>
-                        <div>{item.description}</div>
-                        <div>{item.preview}</div>
-                        <div>{item.source}</div>
+                        <div>
+                            <div>{item.name}</div>
+                            <div>{item.description}</div>
+                            <div>
+                                <a href={item.preview}>Live Preview</a>
+                                <a href={item.source}>Github</a>
+                            </div>
+                        </div>
                         <img src={item.gif} alt={item.name} />
                     </div>
                 );
