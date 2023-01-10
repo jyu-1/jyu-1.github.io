b15b2cffec543cef7d385cc907f5196527a658e3
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-01-09 20:35:20 -0800

    change page styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,36 +6,39 @@ import Carousel from "./Carousel";
 const About = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className="about">
-            <div>
-                <p>
-                    Hi, I'm Jia, an Electrical Engineering graduate from the
-                    University of California, Riverside. When I first learned
-                    about web development, I was fascinated by its similarities
-                    to my studies and the endless possibilities it offers. With
-                    my background in research and collaboration with others, I
-                    bring a strong foundation in problem-solving and critical
-                    thinking to my work in software development.
-                </p>
-                <br />
-                <br />
-                <p>
-                    Driven by curiosity and a desire to explore new
-                    technologies, I'm constantly finding new ways to build new
-                    experiences. Whether I'm experimenting with new frameworks
-                    or diving into the latest APIs, I'm always looking for ways
-                    to push the boundaries of what's possible with web
-                    development.
-                </p>
-                <br />
-                <br />
-                <p>
-                    Outside of coding, I enjoy playing tennis, trying new foods,
-                    and traveling.
-                </p>
-            </div>
-            <div className="about-skills">
-                <Cloud />
-                <Carousel />
+            <div className="about-content">
+                <div>
+                    <p>
+                        Hi, I'm Jia, an Electrical Engineering graduate from the
+                        University of California, Riverside. When I first
+                        learned about web development, I was fascinated by its
+                        similarities to my studies and the endless possibilities
+                        it offers. With my background in research and
+                        collaboration with others, I bring a strong foundation
+                        in problem-solving and critical thinking to my work in
+                        software development.
+                    </p>
+                    <br />
+                    <br />
+                    <p>
+                        Driven by curiosity and a desire to explore new
+                        technologies, I'm constantly finding new ways to build
+                        new experiences. Whether I'm experimenting with new
+                        frameworks or diving into the latest APIs, I'm always
+                        looking for ways to push the boundaries of what's
+                        possible with web development.
+                    </p>
+                    <br />
+                    <br />
+                    <p>
+                        Outside of coding, I enjoy playing tennis, trying new
+                        foods, and traveling.
+                    </p>
+                </div>
+                <div className="about-skills">
+                    <Cloud />
+                    <Carousel />
+                </div>
             </div>
         </div>
     );
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,24 +5,32 @@ import pData from "./data";
 const Project = forwardRef<HTMLDivElement>((props, ref) => {
     return (
         <div ref={ref} className="project">
-            {pData.map((item, index) => {
-                return (
-                    <div
-                        key={index}
-                        className={index % 2 === 0 ? "left-gif" : "right-gif"}
-                    >
-                        <div>
-                            <div className="project-title">{item.name}</div>
-                            <div>{item.description}</div>
-                            <div>
-                                <a href={item.preview}>Live Preview</a>
-                                <a href={item.source}>Github</a>
+            <div className="project-content">
+                {pData.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <div
+                                className={
+                                    index % 2 === 0 ? "left-gif" : "right-gif"
+                                }
+                            >
+                                <div>
+                                    <div className="project-title">
+                                        {item.name}
+                                    </div>
+                                    <div>{item.description}</div>
+                                    <div>
+                                        <a href={item.preview}>Live Preview</a>
+                                        <a href={item.source}>Github</a>
+                                    </div>
+                                </div>
+                                <img src={item.gif} alt={item.name} />
                             </div>
+                            <hr />
                         </div>
-                        <img src={item.gif} alt={item.name} />
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </div>
     );
 });
