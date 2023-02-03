53b3461ec9889a80859a5805ca6085ad86f529fb
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-02-03 05:08:00 -0800

    add initial files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Rebuilt of portfolio with next
+# Portfolio Website
+
+This is Version 2 of the website rebuilt using Next.js.\
+
+## Live Website
+
+https://nateyu.com/
+
+## Link To Version 1 of the Website
+
+https://github.com/jyu-1/jyu-1.github.io/tree/v1
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Nathan | Full Stack Developer</title>
+                <meta
+                    name="description"
+                    content="Full stack developer specializing in MERN Stack. Building modern and interactive apps."
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div>Main</div>
+        </>
+    );
+}
