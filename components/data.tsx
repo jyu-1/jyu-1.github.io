b4fa9f8600ee2f5f13dfba5a0ca750cddbd5fe0e
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2023-02-03 11:07:55 -0800

    build deploy files

--- components/data.tsx
+++ components/data.tsx
@@ -3,7 +3,7 @@ const pData = [
         name: "Feedbook",
         description:
             "Facebook clone. Features user authentication, creating post, deleting post, updating post, commenting on post, and liking post. Built with Next.js, Express.js, Node.js, and MongoDB with Mongoose.",
-        preview: "https://jyu-1.github.io/feedbook/",
+        preview: "https://nateyu.com/feedbook/",
         source: "https://github.com/jyu-1/feedbook",
         gif: "/assets/projects/feedbook.gif",
     },
@@ -11,7 +11,7 @@ const pData = [
         name: "Shopping Cart",
         description:
             "Front-end solution for a shopping website. The store allows users to browse, add, and remove items from the cart. Built using React and React Router.",
-        preview: "https://jyu-1.github.io/shopping-cart/",
+        preview: "https://nateyu.com/shopping-cart/",
         source: "https://github.com/jyu-1/shopping-cart",
         gif: "/assets/projects/shop.gif",
     },
@@ -19,7 +19,7 @@ const pData = [
         name: "Discuss App",
         description:
             "A clone of the popular communication app, Discord. Features include user authentication, channel selection, channel creation, and more. Build with React, React Router, and Firebase.",
-        preview: "https://jyu-1.github.io/discuss/",
+        preview: "https://nateyu.com/discuss/",
         source: "https://github.com/jyu-1/discuss",
         gif: "/assets/projects/discuss.gif",
     },
@@ -27,7 +27,7 @@ const pData = [
         name: "Weather App",
         description:
             "Weather app to check the forecast for the next five days. Features include searching for any city in the world and unit selection. Forecast data is provided by OpenWeatherAPI. Bundled using Webpack.",
-        preview: "https://jyu-1.github.io/weather/",
+        preview: "https://nateyu.com/weather/",
         source: "https://github.com/jyu-1/weather",
         gif: "/assets/projects/weather.gif",
     },
@@ -35,7 +35,7 @@ const pData = [
         name: "Resume Maker",
         description:
             "An easy solution to making a personal resume (CV). Build a resume using the left side of the app, and real-time feedback will be provided on the right. Build using React.",
-        preview: "https://jyu-1.github.io/cv-project/",
+        preview: "https://nateyu.com/cv-project/",
         source: "https://github.com/jyu-1/cv-project",
         gif: "/assets/projects/cv.jpg",
     },
@@ -43,7 +43,7 @@ const pData = [
         name: "Memory Card",
         description:
             "Select a card without picking the same one. Every successful selection earns 1 point. Picking an already picked card resets the point back to zero. Built using React.",
-        preview: "https://jyu-1.github.io/memory-card/",
+        preview: "https://nateyu.com/memory-card/",
         source: "https://github.com/jyu-1/memory-card",
         gif: "/assets/projects/memory.gif",
     },
